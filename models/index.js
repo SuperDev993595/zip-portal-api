7e227b9eee32e2eb8ad4d33187a8645cc7673df7
superdev993595 <jaylen.cooper0826@gmail.com>
2025-08-31 10:48:27 +0800

    Update models index - remove User-Transaction associations for new ZIP structure

--- models/index.js
+++ models/index.js
@@ -2,9 +2,8 @@ const sequelize = require('../config/database');
 const User = require('./User');
 const Transaction = require('./Transaction');
 
-// Define relationships
-User.hasMany(Transaction, { foreignKey: 'userId', sourceKey: 'userId' });
-Transaction.belongsTo(User, { foreignKey: 'userId', targetKey: 'userId' });
+// No associations needed for the new structure
+// Transactions are standalone records without user relationships
 
 module.exports = {
   sequelize,
